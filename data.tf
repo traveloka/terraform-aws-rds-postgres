f155a9c94aa53a6fb0773ebd3772f9d07b8313d9
Rizki Nanda Agam <crazynuxer@gmail.com>
2019-02-04 13:14:31 +0700

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "aws_security_group" "bastion" {
   filter {
     name   = "group-name"
-    values = ["${var.product_domain}bstn-mongod"]
+    values = ["${var.product_domain}bstn-postgres"]
   }
 
   vpc_id = "vpc-32b41f57"
